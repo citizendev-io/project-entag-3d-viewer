bac45d232c612d4a0a56552d40f553f19a522517
trung-nh <trungnh.ict@gmail.com>
2024-11-18 23:13:43 +0700

    rendered: dummy model

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,26 @@ function App() {
       const options = {
         env: "AutodeskProduction",
         accessToken:
-          "eyJhbGciOiJSUzI1NiIsImtpZCI6IlhrUFpfSmhoXzlTYzNZS01oRERBZFBWeFowOF9SUzI1NiIsInBpLmF0bSI6ImFzc2MifQ.eyJzY29wZSI6WyJ2aWV3YWJsZXM6cmVhZCJdLCJjbGllbnRfaWQiOiJCd1FZYzBOaGdhN1FLWkJ3NWdwVWhUdHVhT1ZpckE1RkZDVk1sMWpDQzBIcUpzSUEiLCJpc3MiOiJodHRwczovL2RldmVsb3Blci5hcGkuYXV0b2Rlc2suY29tIiwiYXVkIjoiaHR0cHM6Ly9hdXRvZGVzay5jb20iLCJqdGkiOiJrSFhPSkZHYUZlbFRjajI4TzE2amVKQTRhcGJwZW80eUtBdERlZTR6czN6Q1RBQ1E1TzFBZmVHS3luUURycWpXIiwiZXhwIjoxNzMxNjY2NTMwLCJ1c2VyaWQiOiI0RjYyWDNRMk1FUVNLWkRXIn0.UlkWeR8u1HWONwyj_DhYSbbVqKyqfJDYduij9xEkp0PWv-frqEEj0kt9bsM6U5FFc8jKiJrWV2EHFHQG0_KprfaIiW_OJuBf5HmI1Wmue0p26w_LD4RYWk-kkGeugh2iKFfLWxzawi6qnFD-WDfpVJmCIfXnGAJkVyQ1Jr-nA4n6wJZQabXGvP_B_oDzLq5dUA7Kg3mTNttyM56kxE7HIN-mkortLAckt7eWMrRoFZTB98L_y_USe6HWjqYE_SpdkKchBAPB4Y4hm5VstAxNSFQHDfVHdrsWI4908q9iUWnzHjD6lxaZKyJZryHZYZJrh3IeAOi-wCBkSvsYdUK3Rw",
+          "eyJhbGciOiJSUzI1NiIsImtpZCI6IlhrUFpfSmhoXzlTYzNZS01oRERBZFBWeFowOF9SUzI1NiIsInBpLmF0bSI6ImFzc2MifQ.eyJzY29wZSI6WyJjb2RlOmFsbCIsImRhdGE6d3JpdGUiLCJkYXRhOnJlYWQiLCJidWNrZXQ6Y3JlYXRlIiwiYnVja2V0OmRlbGV0ZSIsImJ1Y2tldDpyZWFkIl0sImNsaWVudF9pZCI6IkJ3UVljME5oZ2E3UUtaQnc1Z3BVaFR0dWFPVmlyQTVGRkNWTWwxakNDMEhxSnNJQSIsImlzcyI6Imh0dHBzOi8vZGV2ZWxvcGVyLmFwaS5hdXRvZGVzay5jb20iLCJhdWQiOiJodHRwczovL2F1dG9kZXNrLmNvbSIsImp0aSI6Im1tSEM5eEZQSUFyMGoyQjBYMVpPazVrdHRiUkMzOUptQWdMSXF6RzVqTWRsaXZUc3BheEZpV3YxY2s5QUxxVmwiLCJleHAiOjE3MzE5NDk5MTZ9.L0HNEsVXlPG6xn347CLFXrTG0hXPgelaoueN0vKoEPUOOArCeMatb3qLHfUtrAgMkG2DgF1-usmiZJO0nRGCDWoQ9XVyZBDgKH3eV6LJgknXCN3nUx6tHoi5QtktSz5Y_O0tmOKng_jzpmgjH-HLVmcyXmGaupJ6UooCk4lRA6nKnTS1ySL0N1SDsfzJG_mzqG4xgNN909zpOoY4eOQB4OcQuOHC7ibF1XGSEd2MIM21EbTj_McUZB8jwNsYC5-eDXrzvXHfWDw75SXiTmm4pajqi8gFX6FJdO4JphDH30Qh5YYcS1lfWwO8ytHTkTF76jmgdpphiDhfGmXFSIc2ug",
       };
 
       // Load Autodesk Viewer
       Autodesk.Viewing.Initializer(options, () => {
-        const viewer = new Autodesk.Viewing.GuiViewer3D(viewerDiv);
+        const viewer = new Autodesk.Viewing.GuiViewer3D(viewerDiv, {});
         viewer.start();
 
         // Load default model
-        const urn = "YOUR_MODEL_URN"; // Replace with a Base64 encoded URN
+        const urn =
+          "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6ZW50YWdidWNrZXRrZXkwMDQvU3VzcGVuc2lvbi56aXA="; // Replace with a Base64 encoded URN
         Autodesk.Viewing.Document.load(
           `urn:${urn}`,
-          (doc: { getRoot: () => { (): any; new(): any; getDefaultGeometry: { (): any; new(): any; }; }; }) => {
+          (doc: {
+            getRoot: () => {
+              (): any;
+              new (): any;
+              getDefaultGeometry: { (): any; new (): any };
+            };
+          }) => {
             const defaultViewable = doc.getRoot().getDefaultGeometry();
             viewer.loadDocumentNode(doc, defaultViewable);
 
@@ -46,12 +53,8 @@ function App() {
       <h1>Entag</h1>
       <div
         id="viewer"
-        style={{ width: "70%", height: "50vh", background: "#f1f1f1" }}
+        style={{ width: "100%", height: "50vh", background: "#f1f1f1" }}
       ></div>
-      <div id="overlay" style={{ position: "absolute", top: 10, left: 10 }}>
-        <select id="models"></select>
-        <button id="explode">Toggle Explode</button>
-      </div>
     </>
   );
 }
