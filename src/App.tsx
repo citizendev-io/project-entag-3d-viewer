2ad56abbc7e80bcd5f9209533859a3f5d6ad3b69
trung-nh <trungnh.ict@gmail.com>
2024-11-19 09:48:57 +0700

    added: URL params for iframe

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import "./App.css";
 
 function App() {
   useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const accessToken = params.get("access-token");
+    const urn = params.get("urn");
+
     // Initialize the Autodesk Viewer
     const initializeViewer = async () => {
       const viewerDiv = document.getElementById("viewer");
       const options = {
         env: "AutodeskProduction",
-        accessToken:
-          "eyJhbGciOiJSUzI1NiIsImtpZCI6IlhrUFpfSmhoXzlTYzNZS01oRERBZFBWeFowOF9SUzI1NiIsInBpLmF0bSI6ImFzc2MifQ.eyJzY29wZSI6WyJjb2RlOmFsbCIsImRhdGE6d3JpdGUiLCJkYXRhOnJlYWQiLCJidWNrZXQ6Y3JlYXRlIiwiYnVja2V0OmRlbGV0ZSIsImJ1Y2tldDpyZWFkIl0sImNsaWVudF9pZCI6IkJ3UVljME5oZ2E3UUtaQnc1Z3BVaFR0dWFPVmlyQTVGRkNWTWwxakNDMEhxSnNJQSIsImlzcyI6Imh0dHBzOi8vZGV2ZWxvcGVyLmFwaS5hdXRvZGVzay5jb20iLCJhdWQiOiJodHRwczovL2F1dG9kZXNrLmNvbSIsImp0aSI6Im1tSEM5eEZQSUFyMGoyQjBYMVpPazVrdHRiUkMzOUptQWdMSXF6RzVqTWRsaXZUc3BheEZpV3YxY2s5QUxxVmwiLCJleHAiOjE3MzE5NDk5MTZ9.L0HNEsVXlPG6xn347CLFXrTG0hXPgelaoueN0vKoEPUOOArCeMatb3qLHfUtrAgMkG2DgF1-usmiZJO0nRGCDWoQ9XVyZBDgKH3eV6LJgknXCN3nUx6tHoi5QtktSz5Y_O0tmOKng_jzpmgjH-HLVmcyXmGaupJ6UooCk4lRA6nKnTS1ySL0N1SDsfzJG_mzqG4xgNN909zpOoY4eOQB4OcQuOHC7ibF1XGSEd2MIM21EbTj_McUZB8jwNsYC5-eDXrzvXHfWDw75SXiTmm4pajqi8gFX6FJdO4JphDH30Qh5YYcS1lfWwO8ytHTkTF76jmgdpphiDhfGmXFSIc2ug",
+        accessToken: accessToken,
       };
 
       // Load Autodesk Viewer
@@ -18,10 +21,8 @@ function App() {
         viewer.start();
 
         // Load default model
-        const urn =
-          "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6ZW50YWdidWNrZXRrZXkwMDQvU3VzcGVuc2lvbi56aXA="; // Replace with a Base64 encoded URN
         Autodesk.Viewing.Document.load(
-          `urn:${urn}`,
+          `urn:${btoa(urn || "")}`,
           (doc: {
             getRoot: () => {
               (): any;
@@ -49,13 +50,10 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h1>Entag</h1>
-      <div
-        id="viewer"
-        style={{ width: "100%", height: "50vh", background: "#f1f1f1" }}
-      ></div>
-    </>
+    <div
+      id="viewer"
+      style={{ width: "100%", height: "50vh", background: "#f1f1f1" }}
+    ></div>
   );
 }
 
