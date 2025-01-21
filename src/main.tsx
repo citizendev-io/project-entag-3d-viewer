4c7bcce17fc7cf836fff585939b20adf1c58ac33
Dat Proto <dat.do@citizendev.io>
2025-01-21 21:57:18 +0700

    debug: Remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Routes } from '@generouted/react-router'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Routes />
-  </StrictMode>,
+  <Routes />
 )
