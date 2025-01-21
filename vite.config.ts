086289bc88060886ed972f1867ae39229cda49b3
Dat Proto <dat.do@citizendev.io>
2025-01-22 03:11:02 +0700

    debug: Update vercel.json (31)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
+import vercel from 'vite-plugin-vercel'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    vercel(),
     react(),
     tsconfigPaths()
   ],
@@ -12,6 +14,13 @@ export default defineConfig({
     headers: {
       'X-Frame-Options': 'ALLOWALL',
       'Content-Security-Policy': "frame-ancestors 'self' *",
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
